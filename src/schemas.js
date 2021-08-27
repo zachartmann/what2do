69394bfd32d4ce97410752e2791f9d50074dd951
Kevin Leung <kevinleung56@gmail.com>
2021-08-27 16:18:27 +1000

    Update Poll schema

--- src/schemas.js
+++ src/schemas.js
@@ -1,16 +1,33 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
-import { string } from "yargs";
+
+/**
+ * Schema definition
+ */
 
 const Poll = new Schema();
 
 Poll.add({
   _id: Number,
   pollId: Number,
-  title: string,
+  title: String,
   endDate: Date,
   timeLimit: Number,
   ideaIds: [Idea],
 });
 
 const Idea = new Schema();
+
+/**
+ * Accessing a specific schema type by key
+ */
+
+/**
+ * Methods
+ */
+
+/**
+ * Define model.
+ */
+
+mongoose.model("Poll", Poll);
