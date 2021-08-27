0c2e247354d199fb8f55608de413cf33ef244075
Kevin Leung <kevinleung56@gmail.com>
2021-08-27 16:07:42 +1000

    Initialise connection and prettier formatting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import './App.css';
-import CreatePollPage from './components/CreatePollPage';
+require("dotenv").config();
+import React from "react";
+import mongoose from "mongoose";
+import "./App.css";
+import CreatePollPage from "./components/CreatePollPage";
 
 const Topic = ({ title }) => {
-  return <h1>{title}</h1>
-}
+  return <h1>{title}</h1>;
+};
 
 const App = () => {
-  const pollId = 'AH32N4'
-  if (pollId === '000000') {
-    return <CreatePollPage />
+  const url = process.env.MONGODB_URI;
+  await mongoose.connect(url); // may need to use createConnection
+
+  const pollId = "AH32N4";
+  if (pollId === "000000") {
+    return <CreatePollPage />;
   } else {
     return (
       <div className="App">
         <Topic title={pollId} />
         <p>Poll deadline</p>
-        <p style={{color: "red"}}>Buy me a coffee! &gt;:(</p>
+        <p style={{ color: "red" }}>Buy me a coffee! &gt;:(</p>
       </div>
-    )
+    );
   }
-}
+};
 
 export default App;
