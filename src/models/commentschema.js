8f394a5ca46c0eaf74c013a77c6674f37ac4a8a9
Juanita999 <jmkoschier@gmail.com>
2021-09-02 17:02:08 +1000

    Adding initial comments schema

---
+++ src/models/commentschema.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+/**
+ * Schema definition
+ */
+
+const Comment = new Schema();
+
+Comment.add({
+  _id: Number,
+  Comment: String,
+  User: String,
+});
+
+/**
+ * Accessing a specific schema type by key
+ */
+
+/**
+ * Methods
+ */
+
+/**
+ * Define model.
+ */
+
+mongoose.model("Comment", comment);
+
