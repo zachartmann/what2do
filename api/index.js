4c649292d90dac1ea0a8a8b9fafed2b794fbc6cf
Kevin Leung <kevinleung56@gmail.com>
2021-09-03 15:54:48 +1000

    Get a sample route working

--- api/index.js
+++ api/index.js
@@ -1,20 +1,20 @@
-import app from "./Server";
+import app from "./server";
 import env from "./environment";
-import db from "./database.js";
+import db from "./database";
 
 // Start the server
 const port = Number(env.port);
 
-async function dbconnect() {
-  try {
-    await db.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-}
+// async function dbconnect() {
+//   try {
+//     await db.authenticate();
+//     console.log("Connection has been established successfully.");
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error);
+//   }
+// }
 
-dbconnect();
+// dbconnect();
 
 app.listen(port, () => {
   console.info("Express server started on port: " + port);
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { StatusCodes } from "http-status-codes";
 
 const router = Router();
 
 // Add sub-routes
 router.get("/hi", async (req, res) => {
-  return res.status(OK).json("Hello friend!")
-};
+  return res.status(StatusCodes.OK).json("Hello friend!");
+});
 
 export default router;
