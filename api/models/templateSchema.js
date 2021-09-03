7deb482eb2ae193770d9767d7f98d19febdf6fd4
Zachary Hartmann <hartmann.zachary@gmail.com>
2021-09-03 16:40:55 +1000

    Re-adding templateschema and fixing yarn.lock

---
+++ api/models/templateSchema.js
@@ -0,0 +1,28 @@
+/**
+ * Schema definition
+ */
+ const Template = new Schema();
+
+ Template.add({
+   title: String,
+   category: String,
+   theme: String,
+   timeLimit: Number,
+   ideaIds: [Number],
+ });
+ 
+ 
+ /**
+  * Accessing a specific schema type by key
+  */
+ 
+ /**
+  * Methods
+  */
+ 
+ /**
+  * Define model.
+  */
+ 
+ export default Template;
+ 
