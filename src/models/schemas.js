9e846c10a0041aeee5371ecf923057a7c7ac2081
zhartmann <52366726+zachartmann@users.noreply.github.com>
2021-09-03 15:40:58 +1000

    Fixing schemas.js
    Resolved conflict wrong way

--- src/models/schemas.js
+++ src/models/schemas.js
@@ -2,33 +2,4 @@ import mongoose from "mongoose";
 import { Schema } from "mongoose";
 import Poll from "./pollSchema";
 
-/**
- * Schema definition
- */
-
-const Poll = new Schema();
-
-Poll.add({
-  _id: Number,
-  pollId: Number,
-  title: String,
-  endDate: Date,
-  timeLimit: Number,
-  ideaIds: [Idea],
-});
-
-// const Idea = new Schema(); new idea should be in a seperate module
-
-/**
- * Accessing a specific schema type by key
- */
-
-/**
- * Methods
- */
-
-/**
- * Define model.
- */
-
 mongoose.model("Poll", Poll);
