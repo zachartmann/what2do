e6a4ee6706550e80b390b586dbab8ea85c0ccdcf
Kevin Leung <kevinleung56@gmail.com>
2021-09-04 12:21:02 +1000

    Get connection working

--- api/index.js
+++ api/index.js
@@ -1,20 +1,11 @@
 import app from "./server";
 import env from "./environment";
-import db from "./database";
+import connect from "./database";
 
 // Start the server
 const port = Number(env.port);
 
-// async function dbconnect() {
-//   try {
-//     await db.authenticate();
-//     console.log("Connection has been established successfully.");
-//   } catch (error) {
-//     console.error("Unable to connect to the database:", error);
-//   }
-// }
-
-// dbconnect();
+connect();
 
 app.listen(port, () => {
   console.info("Express server started on port: " + port);
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,31 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
+import PollModel from "../models/pollSchema";
 
 const router = Router();
 
 // Add sub-routes
 router.get("/hi", async (req, res) => {
   return res.status(StatusCodes.OK).json("Hello friend!");
+});
+
+// Add sub-routes
+router.get("/poll", async (req, res) => {
+  const pollToCreate = new PollModel({
+    pollId: 1,
+    title: "Test",
+    endDate: new Date(),
+    timeLimit: 10,
+    ideaIds: [1],
+  });
+  pollToCreate.save((err) => {
+    if (err) {
+      console.log("Something didn't work");
+    } else {
+      console.log("Something worked!");
+    }
+  });
+  return res.status(StatusCodes.OK).json("Hello friend! I saved your dummy");
 });
 
 export default router;
