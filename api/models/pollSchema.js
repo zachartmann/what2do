f99c2448ec24499741fe89440c0318fa38237fd1
Kevin Leung <kevinleung56@gmail.com>
2021-09-04 11:03:32 +1000

    Add poll schema from ERD

---
+++ api/models/pollSchema.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+/**
+ * Schema definition
+ */
+const Poll = new Schema();
+
+Poll.add({
+pollId: Number,
+title: String,
+endDate: Date,
+timeLimit: Number,
+ideaIds: [Number],
+});
+
+/**
+ * Accessing a specific schema type by key
+ */
+
+/**
+ * Methods
+ */
+
+/**
+ * Define model.
+ */
+mongoose.model("Poll", Poll);
+ 
+export default Poll;
