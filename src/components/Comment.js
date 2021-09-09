9ed7aa00c1514910d75100ddeb5d404f6aeedfbc
Juanita999 <jmkoschier@gmail.com>
2021-09-09 21:34:13 +1000

    New idea box has been created which is planned to include a comment box AB#88

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,10 +1,43 @@
 import React from "react";
 
+import Idea from "./Idea"
 
-/*import Info from "../components/Info"; */
+/* const Comment = ({ text, downvotes, upvotes }) => { */
+  const Comment = () => {
+  /* return <p>Hello Testing that this is working yay</p> */
 
-const Comment = ({ text, downvotes, upvotes }) => {
-  return <p>Hello Testing that this is working yay</p>
-}       
+    const ideas = [
+      {
+      content: "Let's create an idea that has a comment box",
+      upVotes: 100,
+      downVotes: 2,
+      upVoters: [
+        {
+          name: "Juanita",
+          password: ""
+        }
+      ],
+      downVoters: [
+        {
+          name: "Juanitaa",
+          password: "1234"
+        },
+        {
+          name: "Juanitaaa",
+          password: ""
+        }
+      ],
+      pinned: false,
+      user: "Juanita"
+    }
+  ] 
+  return (
+    <>
+      {ideas.map(idea => {
+        return <Idea idea={idea} />
+      })}
+    </>
+  )
+}     
 
 export default Comment;
