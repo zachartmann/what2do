632d23b24e65f70cc4a02443646b86195e6641d7
zhartmann <hartmann.zachary@gmail.com>
2021-09-09 12:20:54 +1000

    Added idea-submission UI to poll page and fixed icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,15 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <p>Account</p>
-      <img alt="Account" className="icon" src="https://upload.wikimedia.org/wikipedia/commons/0/03/User_icon.svg" />
+      <p>
+        <b>what2do.com/ABCDEF</b>
+        <svg className="h-6 w-6 icon header-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+        </svg>
+        <svg className="h-6 w-6 icon header-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+        </svg>
+      </p>
     </div>
   )
 };
--- src/components/Ideas.js
+++ src/components/Ideas.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Ideas = () => {
-  const ideas = []
+  // const ideas = []
   return (
     <>
       {/* {ideas.map(idea => {
