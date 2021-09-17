77845dc76921eb29919f54b86470f5484d6d259c
Juanita999 <jmkoschier@gmail.com>
2021-09-17 12:11:01 +1000

    Testing adding an icon AB#90

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -8,26 +8,31 @@ const Comment = ({ comment }) => {
     }
 
     return (
-        <div className="content-container idea">
-        <div className="content-container flex-container">
-            <div className="flex-component flex-70 flex-container">
-            <div className="flex-compoment flex-70">
-                <input placeholder="Enter your comment"></input>
+        <div className="commentbox">
+            <div className="content-container comment">
+            <div className="content-container flex-container">
+                <div className="flex-component flex-70 flex-container">
+                <div className="flex-compoment flex-30">
+                    <input placeholder="Enter your comment"></input>
+                </div>
+                </div>
             </div>
+            <div className="content-container flex-container">
+                <div className="flex-component flex-70 flex-container">
+                <div className="flex-compoment flex-30">
+                    <input placeholder={comment.commentInput}></input>
+                </div>
+                <svg className="h-6 w-6 icon" xmlns="http://www.w3.org/2000/svg" fill="lightblue" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
+                </svg>
+                </div>
             </div>
-        </div>
-        <div className="content-container flex-container">
-            <div className="flex-component flex-70 flex-container">
-            <div className="flex-compoment flex-70">
-                <input placeholder={comment.commentInput}></input>
+            <div class="clickbuttonpopuphappens">
+                    <button onClick={buttonPopup}>ButtonClick</button>
             </div>
+            {/*}
+            < CommentBox />*/}
             </div>
-        </div>
-        <div class="clickbuttonpopuphappens">
-                <button onClick={buttonPopup}>ButtonClick</button>
-        </div>
-        {/*}
-        < CommentBox />*/}
         </div>
     )  
 }
