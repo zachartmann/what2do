2fba72392864eac0fa30d49a5c045cae4127e963
zhartmann <hartmann.zachary@gmail.com>
2021-09-17 14:58:28 +1000

    Info popup html

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,12 +1,15 @@
 import React from "react";
 
+import InfoPopup from "./InfoPopup";
+
 const Info = ({ text }) => {
   return (
-    <div className="info">
+    <>
       <svg className="h-6 w-6 icon blue-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
-    </div>
+      <InfoPopup />
+    </>
   )
 };
 
---
+++ src/components/InfoPopup.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InfoPopup = () => {
+  return (
+    <div className="content popup info-popup hidden">
+      <div className="content-container">
+        <p>Logging in is not necessary to create a poll or respond to one. We use it to ensure continuity between sessions though, so we recommend using one.</p>
+      </div>
+    </div>
+  )
+}
+
+export default InfoPopup;
