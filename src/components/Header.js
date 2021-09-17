6aed83485d956ad2b214991aa01d83e2125e6c1f
zhartmann <hartmann.zachary@gmail.com>
2021-09-17 15:13:24 +1000

    Updated SVG paths and removed p > div

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,27 +8,27 @@ const Header = ({ pollId }) => {
     // Show the create-poll UI since there's no ID
     return (
       <div className="header">
-        <p>
+        <div className="inline">
           <b>Welcome to what2do!</b>
           <svg className="h-6 w-6 icon blue-icon header-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
           </svg>
-          <Account />
-        </p>
+        <Account />
+        </div>
       </div>
     )
   } else {
     // Show pollId and ability to copy link
     return (
       <div className="header">
-        <p>
+        <div className="inline">
           <b>what2do.com/{pollId}</b>
           <button>Copy Link</button>
           <svg className="h-6 w-6 icon blue-icon header-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
           </svg>
           <Account />
-        </p>
+        </div>
       </div>
     )
   }
--- src/components/Info.js
+++ src/components/Info.js
@@ -6,7 +6,7 @@ const Info = ({ text }) => {
   return (
     <>
       <svg className="h-6 w-6 icon blue-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
       <InfoPopup />
     </>
