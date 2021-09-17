df3352a84ee9e3613869f3a5408ef91d5381f8b4
zhartmann <hartmann.zachary@gmail.com>
2021-09-17 15:31:23 +1000

    Show shadow on header icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ pollId }) => {
       <div className="header">
         <div className="inline">
           <b>Welcome to what2do!</b>
-          <svg className="h-6 w-6 icon blue-icon header-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <svg className="h-6 w-6 icon blue-icon header-icon button-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
           </svg>
         <Account />
@@ -24,7 +24,7 @@ const Header = ({ pollId }) => {
         <div className="inline">
           <b>what2do.com/{pollId}</b>
           <button>Copy Link</button>
-          <svg className="h-6 w-6 icon blue-icon header-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <svg className="h-6 w-6 icon blue-icon header-icon button-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
           </svg>
           <Account />
