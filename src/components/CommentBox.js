aa9a1a281eb1776d8e13217130ceb91965331417
Juanita999 <jmkoschier@gmail.com>
2021-09-20 19:16:56 +1000

    Basic comment functionality ui need some cleanup AB#90

---
+++ src/components/CommentBox.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+
+function Card(props) {
+  return (
+    <div className="Card">
+      Name: {props.name}
+      <br />
+      Address: {props.address}
+      <br />
+      Comment: {props.comment}
+      <button onClick={() => props.delete(props.comment)}>Delete</button>
+      <br />
+    </div>
+  );
+}
+
+export default function App() {
+  const [sim, setSim] = useState([]);
+  const [components, setComponents] = useState({
+    comment: "",
+    name: "",
+    address: ""
+  });
+
+  const handleComment = () => {
+    const temp = [
+      ...sim,
+      {
+        comment: components.comment,
+        name: components.name,
+        address: components.address
+      }
+    ];
+    setSim(temp);
+    setComponents({ comment: "", name: "", address: "" });
+  };
+
+  const del = (comment) => {
+    console.log(comment, "deleted");
+    const newArr = sim.filter((element) => {
+      return element.comment !== comment;
+    });
+    setSim(newArr);
+  };
+
+  return (
+    <div className="App">
+      <div className="name">
+        {" "}
+        Name:
+        <input
+          type="text"
+          value={components.name}
+          onChange={(event) => {
+            const temp = {
+              ...components,
+              name: event.target.value
+            };
+            setComponents(temp);
+          }}
+        />
+      </div>
+      <br />
+      Address:
+      <input
+        type="text"
+        value={components.address}
+        onChange={(event) => {
+          const temp = {
+            ...components,
+            address: event.target.value
+          };
+          setComponents(temp);
+        }}
+      />
+      <br />
+      Write your comments here:
+      <br />
+      <textarea
+        rows="15"
+        cols="25"
+        value={components.comment}
+        onChange={(event) => {
+          const temp = {
+            ...components,
+            comment: event.target.value
+          };
+          setComponents(temp);
+        }}
+      />
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          handleComment();
+        }}
+      >
+        Add comment
+      </button>
+      {sim.map((element) => {
+        return (
+          <Card
+            comment={element.comment}
+            name={element.name}
+            address={element.address}
+            delete={del}
+          />
+        );
+      })}
+    </div>
+  );
+}
