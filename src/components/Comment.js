73a55c6f0aea836289e80c659a1e2fae6a047dfc
Juanita999 <jmkoschier@gmail.com>
2021-09-21 16:16:10 +1000

    Cleaning up the comments format AB#90 AB#77

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,8 @@
 import React from "react";
 import CommentBox from "../components/CommentBox";
 
-const Comment = ({ comment }) => {
+const Comment = ({ }) => {
+//const Comment = ({ comment }) => {
 
     function buttonPopup() {
         alert('This button will mean something eventually maybe to see more comments');
@@ -9,18 +10,18 @@ const Comment = ({ comment }) => {
 
     return (
         <div className="commentbox">
+            < CommentBox />
             <div className="content-container comment">
-            <div className="content-container flex-container">
+            {/*<div className="content-container flex-container">
                 <div className="flex-component flex-70 flex-container">
                 <div className="flex-compoment flex-30">
                     <input placeholder={comment.commentInput}></input>
                 </div>
                 </div>
-            </div>
+            </div>*/}
             <div class="clickbuttonpopuphappens">
                     <button onClick={buttonPopup}>ButtonClick</button>
             </div>
-            < CommentBox />
             </div>
         </div>
     )  
