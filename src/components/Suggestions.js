e9692692f6e3ed8f23fac90c029c2c878c18b89c
Yash Parge <yash_parge@yahoo.com.au>
2021-09-26 19:13:56 +1000

    Linked suggestions with IdeaSubmission

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,25 +1,21 @@
-import React from "react";
 
-const Suggestions = () => {
-  //Suggestion: String
-  //Category: String
+const Suggestions = [
+  {
+    suggestion: "Lets go to the zoo",
+    category: "where2go",
+  },
+  {
+    suggestion: "Lets eat at maccas",
+    category: "where2go",
+  },
+  {
+    suggestion: "Lets play tic tac toe",
+    category: "what2play",
+  },
+  {
+    suggestion: "Lets go to sleep",
+    category: "what2do",
+  },
+];
 
-  const suggestions = [
-    {
-      suggestion: "Lets go to the zoo",
-      category: "where2go",
-    },
-    {
-      suggestion: "Lets eat at maccas",
-      category: "where2go",
-    },
-    {
-      suggestion: "Lets play tic tac toe",
-      category: "what2play",
-    },
-    {
-      suggestion: "Lets go to sleep",
-      category: "what2do",
-    },
-  ];
-};
+export default Suggestions;
