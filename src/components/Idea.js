e0d428c2ae81b8279c45c8fcaf281e8c34d95f2d
seant1 <sean.tran@outlook.com.au>
2021-10-01 20:50:53 +1000

    Add pin star button on idea hover

--- src/components/Idea.js
+++ src/components/Idea.js
@@ -34,6 +34,7 @@ const Idea = ({ idea }) => {
     createdAt,
   } = idea;
   let metaLabel;
+  const [pinHidden, setPinHidden] = useState(true);
 
   // Create the 'edited/created at' tag that shows when last edited/created
   if (lastModified) {
@@ -64,11 +65,43 @@ const Idea = ({ idea }) => {
     setHidden(!hidden);
   };
 
+  const handleMouseEnter = () => {
+    setPinHidden(false);
+  };
+  
+  const handleMouseLeave = () => {
+    setPinHidden(true);
+  };
+
   const commentFill = hidden ? "white" : "lightskyblue";
 
   return (
     <div className="content">
-      <div className="content-container idea">
+      <div
+        className="content-container idea"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        {!pinHidden ? (
+          <div style={{ height: 0 }}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon blue-icon header-icon button-icon"
+              style={{ position: "relative", top: "-20" }}
+              fill="yellow"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+              />
+            </svg>
+          </div>
+        ) : null}
+
         <div
           className="content-container flex-container"
           style={{ paddingBottom: "0px" }}
