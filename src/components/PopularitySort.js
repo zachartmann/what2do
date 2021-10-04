5868b5593651349b39e5ef94d0851d6e45f4d7a0
Juanita999 <jmkoschier@gmail.com>
2021-10-04 17:01:09 +1100

    Attempt at sorting ideas, this does not work AB#36 AB#38

--- src/components/PopularitySort.js
+++ src/components/PopularitySort.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
-const PopularitySort = () => {
+import Articles from "../components/Articles.js";
+
+const PopularitySort = ({ articles }) => {
+  const [sort, setSort] = useState(1);
+
+  const newArticles = useMemo(() => {
+    if (sort === 2) {
+      return articles.sort((a, b) =>
+        a.date < b.date ? 1 : b.date < a.date ? -1 : 0
+      );
+    }
+  }, [articles, sort]);
+
   return (
     <div className="content">
       <div classname="thisclass">
         <button> Popularity </button>
         <button> Recency </button>
+        <button data-testid="most-upvoted-link" onClick={() => setSort(1)}>
+          Most Upvoted
+        </button>
+        <button data-testid="most-recent-link" onClick={() => setSort(2)}>
+          Most Recent
+        </button>
+      </div>
+      <div>
+        <Articles articles={newArticles} />
       </div>
     </div>
   );
