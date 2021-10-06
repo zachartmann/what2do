b488c3f43480563b7d247c7948a234b599a681b5
Juanita999 <jmkoschier@gmail.com>
2021-10-06 18:06:03 +1100

    Updating Readme and commenting the code as per the Assessment Requirements AB#36 AB#38

--- src/components/Ideas.js
+++ src/components/Ideas.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 
+// This component contains the logic to sort the ideas by two parameters which are createdAt and upVotes (date in which the idea was created, and the number of upVotes)
+// This component also contains the logic for the idea pinning functionality
+
 import "./Ideas.css";
 
 import Idea from "./Idea";
