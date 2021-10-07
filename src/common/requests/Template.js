05767aa173051fda76c02cc2f19fd12f44c97bf8
zachartmann <hartmann.zachary@gmail.com>
2021-10-07 11:45:32 +1100

    Setup common request for templates

--- src/common/requests/Template.js
+++ src/common/requests/Template.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { templatesEndpoint } from "./Endpoints";
+import { templateEndpoint, templatesEndpoint } from "./Endpoints";
 
 /**
  * Templates
@@ -8,6 +8,30 @@ import { templatesEndpoint } from "./Endpoints";
 export async function getTemplates() {
   try {
     const response = await axios.get(templatesEndpoint);
+    return response;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function getTemplate(templateId) {
+  try {
+    const response = await axios.get(`${templateEndpoint}/${templateId}`);
+    return response;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function postTemplate(title, category, theme, timeLimit, ideaIds) {
+  try {
+    const response = await axios.post(templateEndpoint, {
+      title,
+      category,
+      theme,
+      timeLimit,
+      ideaIds,
+    });
     return response;
   } catch (err) {
     throw err;
