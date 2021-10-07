badbb325410641b408f638801846b7c59a11254b
zachartmann <hartmann.zachary@gmail.com>
2021-10-07 15:32:44 +1100

    Revert bootstrap and try to replicate it... keyword: try

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -11,87 +11,51 @@ const FeedbackForm = (props) => {
     postFeedback(feedbackContent);
   };
   return (
-    <section class="ftco-section">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-12">
-            <div class="wrapper">
-              <div class="row no-gutters">
-                <div class="col-lg-8 col-md-7 order-md-last d-flex align-items-stretch">
-                  <div class="contact-wrap w-100 p-md-5 p-4">
-                    <h3 class="mb-4">Send us feedback!</h3>
-                    <div id="form-message-warning" class="mb-4"></div>
-                    <div id="form-message-success" class="mb-4">
-                      Your message was sent, thank you!
-                    </div>
-                    <form
-                      onSubmit={handleSubmit}
-                      id="feedbackForm"
-                      name="feedbackForm"
-                      class="contactForm"
-                    >
-                      <div class="row">
-                        <div class="col-md-12">
-                          <div class="form-group">
-                            <label class="label" for="#">
-                              Message
-                            </label>
-                            <textarea
-                              onChange={(e) => setFeedback(e.target.value)}
-                              name="content"
-                              class="form-control"
-                              id="message"
-                              cols="30"
-                              rows="5"
-                              placeholder="Message"
-                            ></textarea>
-                          </div>
-                        </div>
-                        <div class="col-md-12">
-                          <div class="form-group">
-                            <input
-                              type="submit"
-                              value="Send Message"
-                              class="btn btn-primary"
-                            ></input>
-                            <div class="submitting"></div>
-                          </div>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-                <div class="col-lg-4 col-md-5 d-flex align-items-stretch">
-                  <div class="info-wrap bg-primary w-100 p-md-5 p-4">
-                    <h3>What2Do Polls</h3>
-                    <p class="mb-4">
-                      We're open for any suggestions or feedback!
-                    </p>
-                    <div class="dbox w-100 d-flex align-items-center">
-                      <div class="text pl-3">
-                        <p>
-                          <span>Email:</span>{" "}
-                          <a href="mailto:info@yoursite.com">
-                            info@yoursite.com
-                          </a>
-                        </p>
-                      </div>
-                    </div>
-                    <div class="dbox w-100 d-flex align-items-center">
-                      <div class="text pl-3">
-                        <p>
-                          <span>Website</span> <a href="#">what2do.com</a>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+    <div className="content feedback-box">
+      <div className="flex-container">
+        <div className="flex-container flex-30 feedback-contact">
+          <div className="content-container">
+            <h3>What2Do Polls</h3>
+            <p>We're open for any suggestions or feedback!</p>
+            <div className="content-container">
+              <p>
+                <span>Email: </span>
+                <a href="mailto:info@yoursite.com">info@yoursite.com</a>
+              </p>
+            </div>
+            <div className="content-container">
+              <p>
+                <span>Website: </span>
+                <a href="#">what2do.com</a>
+              </p>
             </div>
           </div>
         </div>
+        <div className="flex-container flex-70">
+          <div className="content-container feedback">
+            <div className="content-container">
+              <h1>Send us feedback!</h1>
+              <h5>MESSAGE</h5>
+            </div>
+            <form onSubmit={handleSubmit} className="feedback-form">
+              <div className="content-container">
+                <textarea
+                  className="feedback-message"
+                  cols="30"
+                  name="content"
+                  onChange={(e) => setFeedback(e.target.value)}
+                  placeholder="Message"
+                  rows="5"
+                ></textarea>
+                <button type="submit" className="submit-feedback">
+                  Send Message
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 // Set default as light theme - I think it looks better :P
 document.addEventListener("DOMContentLoaded", () => {
