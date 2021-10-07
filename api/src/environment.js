7a278f0aac16d446f1fe05a69888ed38a0caf6c7
Kevin Leung <kevinleung56@gmail.com>
2021-10-07 22:08:34 +1100

    Try fix deployment port issue

--- api/src/environment.js
+++ api/src/environment.js
@@ -3,6 +3,6 @@ dotenv.config();
 
 export default {
   dbUri: process.env.MONGODB_URI ?? "",
-  port: process.env.PORT ?? "3000",
+  port: process.env.PORT ?? "8080",
   node_env: process.env.NODE_ENV ?? "development",
 };
