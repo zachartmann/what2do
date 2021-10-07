f83fd089ea544abbeea64e5d515cc1710dff29e5
Kevin Leung <kevinleung56@gmail.com>
2021-10-08 10:07:37 +1100

    Only allow editing if local name is the same as the idea user name

--- src/components/Idea.js
+++ src/components/Idea.js
@@ -199,25 +199,28 @@ const Idea = ({ idea }) => {
             </p>
           </div>
           <div className="flex-component flex-70 flex-end">
-            <svg
-              style={{ paddingRight: "7px", paddingBottom: "15px" }}
-              onClick={() => {
-                setShowEdit(!showEdit);
-              }}
-              class="w-6 h-6"
-              fill="none"
-              width="24px"
-              height="24px"
-              stroke="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-              ></path>
-            </svg>
+            {localStorage.getItem("user") &&
+              localStorage.getItem("user") == user && (
+                <svg
+                  style={{ paddingRight: "7px", paddingBottom: "15px" }}
+                  onClick={() => {
+                    setShowEdit(!showEdit);
+                  }}
+                  class="w-6 h-6"
+                  fill="none"
+                  width="24px"
+                  height="24px"
+                  stroke="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                  ></path>
+                </svg>
+              )}
             <svg
               onClick={toggleComments}
               className="h-6 w-6 icon blue-icon comment-icon button-icon"
