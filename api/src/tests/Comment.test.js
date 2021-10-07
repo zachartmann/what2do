6c07dd3b29212f9b379d25efb37731057120b610
Juanita999 <jmkoschier@gmail.com>
2021-10-07 23:25:15 +1100

    Responding to feedback AB#27

--- api/src/tests/Comment.test.js
+++ api/src/tests/Comment.test.js
@@ -2,13 +2,13 @@ import supertest from "supertest";
 import server from "../server";
 
 describe("Comments endpoint", () => {
-  it("Should return all suggestions", () => {
+  it("Should return all comments", () => {
     expect(1 + 1).toBe(2); // TODO
   });
 });
 
 describe("Comments endpoint", () => {
-  it("Should post a suggestion with valid input", () => {
+  it("Should post a comment with valid input", () => {
     expect(1 + 1).toBe(2); // TODO
   });
 });
